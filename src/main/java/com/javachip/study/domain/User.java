f8808df64f3c5fd996509386a60b845252098716
NuGuri03 <sym2596@knu.ac.kr>
2025-05-24 20:53:40 +0900

    refactor: lombok 삭제

--- src/main/java/com/javachip/study/domain/User.java
+++ src/main/java/com/javachip/study/domain/User.java
@@ -2,16 +2,11 @@ package com.javachip.study.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 //유저 도메인 객체(DB)
 //데이터베이스와 매칭하기 위한 클래스
 //생성자 주입 사용
 @Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     private Long studentId;
@@ -25,5 +20,18 @@ public class User {
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
     }
 }
