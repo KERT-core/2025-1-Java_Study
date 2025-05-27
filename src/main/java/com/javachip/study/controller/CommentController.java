0477274730796ad7a70cc4f99e294fddd2ee13e8
Mickey777777 <wh879642@gmail.com>
2025-05-28 01:54:37 +0900

    RestController, RequestMapping 으로 재구성

--- src/main/java/com/javachip/study/controller/CommentController.java
+++ src/main/java/com/javachip/study/controller/CommentController.java
@@ -2,14 +2,35 @@ package com.javachip.study.controller;
 
 import com.javachip.study.dto.CommentDto;
 import com.javachip.study.service.CommentService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts/{postId}/comments")
 public class CommentController {
+
     private final CommentService commentService;
-    public CommentController(CommentService commentService) { this.commentService = commentService; }
-    public CommentDto createComment(CommentDto dto) { return commentService.create(dto);}
-    public List<CommentDto> listComments() { return commentService.getAll();}
-    public CommentDto getCommentById(Long id) { return commentService.getById(id);}
-    public void delete(Long id) { commentService.delete(id);}
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public CommentDto createComment(@PathVariable Long postId, @RequestBody CommentDto dto) {
+        return commentService.create(dto);
+    }
+    @GetMapping
+    public List<CommentDto> listComments(@PathVariable Long postId) {
+        return commentService.getAll();
+    }
+    @GetMapping("/{id}")
+    public CommentDto getCommentById(@PathVariable Long postId, @PathVariable Long id) {
+        return commentService.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        commentService.delete(id);
+    }
 }
