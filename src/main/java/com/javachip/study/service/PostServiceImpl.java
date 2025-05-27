8594cee578e47160aa5031b10d97a2885dedfa1a
Mickey777777 <wh879642@gmail.com>
2025-05-28 01:26:16 +0900

    getStudentId()로 수정

--- src/main/java/com/javachip/study/service/PostServiceImpl.java
+++ src/main/java/com/javachip/study/service/PostServiceImpl.java
@@ -33,7 +33,7 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto create(PostDto dto) {
         // 1) 사용자 조회 (Optional.ofNullable → orElseThrow)
-        UserEntity user = Optional.ofNullable(userRepo.findById(dto.userId()))
+        UserEntity user = Optional.ofNullable(userRepo.findByStudentId(dto.userId()))
                 .orElseThrow(() -> new UserNotFoundException(dto.userId()));
 
         // 2) 엔티티 변환 & 저장
