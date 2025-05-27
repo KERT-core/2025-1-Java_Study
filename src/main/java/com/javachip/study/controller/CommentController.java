e124ef398603dc6050ce5924cae74e2a5cfae273
Mickey777777 <wh879642@gmail.com>
2025-05-28 01:24:15 +0900

    CommentController 구현

--- src/main/java/com/javachip/study/controller/CommentController.java
+++ src/main/java/com/javachip/study/controller/CommentController.java
@@ -1,4 +1,15 @@
 package com.javachip.study.controller;
 
+import com.javachip.study.dto.CommentDto;
+import com.javachip.study.service.CommentService;
+
+import java.util.List;
+
 public class CommentController {
+    private final CommentService commentService;
+    public CommentController(CommentService commentService) { this.commentService = commentService; }
+    public CommentDto createComment(CommentDto dto) { return commentService.create(dto);}
+    public List<CommentDto> listComments() { return commentService.getAll();}
+    public CommentDto getCommentById(Long id) { return commentService.getById(id);}
+    public void delete(Long id) { commentService.delete(id);}
 }
