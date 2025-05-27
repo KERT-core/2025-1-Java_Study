4810846e29e1dcb3d87ad8c1e73a12689e123ac2
Mickey777777 <wh879642@gmail.com>
2025-05-27 10:11:22 +0900

    CommentService 구현

---
+++ src/main/java/com/javachip/study/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.javachip.study.service;
+
+import com.javachip.study.dto.CommentDto;
+import java.util.List;
+
+public interface CommentService {
+    CommentDto create(CommentDto commentDto);
+    List<CommentDto> findAll();
+    CommentDto findById(Long id);
+    CommentDto update(CommentDto commentDto);
+    void delete(Long id);
+}
