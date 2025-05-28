a975cb20e125349a99ca675975fddd1a1b8a4c46
Giwon <jkw04257773@gmail.com>
2025-05-28 15:03:50 +0900

    chore: EventNotFound 오타 수정

--- src/main/java/com/javachip/study/exception/EventNotFoundException.java
+++ src/main/java/com/javachip/study/exception/EventNotFoundException.java
@@ -1,5 +1,6 @@
 package com.javachip.study.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
