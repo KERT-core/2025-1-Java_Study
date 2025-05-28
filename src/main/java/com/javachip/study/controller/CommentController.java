8f11364a83d310c9ea9e4fd9be395bdb322d32f8
Mickey777777 <wh879642@gmail.com>
2025-05-28 17:02:55 +0900

    feat: 응답 실패 구현

--- src/main/java/com/javachip/study/controller/CommentController.java
+++ src/main/java/com/javachip/study/controller/CommentController.java
@@ -3,6 +3,8 @@ package com.javachip.study.controller;
 import com.javachip.study.dto.CommentDto;
 import com.javachip.study.response.ApiResponse;
 import com.javachip.study.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +20,32 @@ public class CommentController {
     }
 
     @PostMapping
-    public ApiResponse<CommentDto> createComment(@PathVariable Long postId, @RequestBody CommentDto dto) {
+    public ResponseEntity<ApiResponse<CommentDto>> createComment(@PathVariable Long postId, @RequestBody CommentDto dto) {
         CommentDto result = commentService.create(dto);
-        return ApiResponse.success(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @GetMapping
-    public ApiResponse<List<CommentDto>> listComments(@PathVariable Long postId) {
+    public ResponseEntity<ApiResponse<List<CommentDto>>> listComments(@PathVariable Long postId) {
         List<CommentDto> result = commentService.getAllByPostId(postId);
-        return ApiResponse.success(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
+
     @GetMapping("/{id}")
-    public ApiResponse<CommentDto> getCommentById(@PathVariable Long postId, @PathVariable Long id) {
-        return ApiResponse.success(commentService.getById(id));
+    public ResponseEntity<ApiResponse<CommentDto>> getCommentById(@PathVariable Long postId, @PathVariable Long id) {
+        try{
+            CommentDto result = commentService.getById(id);
+            return ResponseEntity.ok(ApiResponse.success(result));
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ApiResponse.fail(e, 404));
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long postId, @PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long postId, @PathVariable Long id) {
         commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(ApiResponse.success(null));
     }
 }
