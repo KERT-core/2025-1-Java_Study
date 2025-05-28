ab6effe1200b706bf020bb1739185c5bfd976fa5
Giwon <jkw04257773@gmail.com>
2025-05-28 15:02:03 +0900

    chore: EventNotFound Error BAD_REQUEST로 수정

--- src/main/java/com/javachip/study/exception/EventNotFoundException.java
+++ src/main/java/com/javachip/study/exception/EventNotFoundException.java
@@ -1,5 +1,8 @@
 package com.javachip.study.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EventNotFoundException extends RuntimeException {
     public EventNotFoundException(Long id) {
         super("Event with ID [" + id + "] not found.");
